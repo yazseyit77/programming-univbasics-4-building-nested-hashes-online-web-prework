7e8f8a05ae7adf585999bf72ee45bc1479033715
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-03 18:14:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
-  base_hash = {:key => "value"}
+  base_hash = {:key => value}
   # keyword "return." Nice and neat.
 	base_hash
 end
